771f9f1591ce402c4a05b0eea2fbf3cb5c199e1f
Avinash Saroj <68055267+avinashsaroj@users.noreply.github.com>
2021-10-24 22:28:16 +0530

    Update server.ino

--- server/server.ino
+++ server/server.ino
@@ -1,15 +1,4 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-client-server-wi-fi/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
 
-// Import required libraries
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
 
